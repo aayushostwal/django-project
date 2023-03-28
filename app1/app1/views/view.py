b1fd06e1112900e23c4686273c90293efd2b49eb
Aayush Ostwal <aayushostwal99@gmail.com>
2023-03-28 22:23:20 +0530

    created Django APP

---
+++ app1/app1/views/view.py
@@ -0,0 +1,22 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+
+import time
+
+
+def internal_function_1():
+    time.sleep(2)
+
+
+def internal_function_2():
+    time.sleep(4)
+
+
+class MyAPIView(APIView):
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        internal_function_1()
+        internal_function_2()
+        return Response({"success": True}, status=200)
